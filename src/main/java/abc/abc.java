bfde5feb5ac4388cbb50062049a7f7fa29ff655e
bca <techinha221@naver.com>
2021-06-20 14:21:56 +0900

    new work2

--- src/main/java/abc/abc.java
+++ src/main/java/abc/abc.java
@@ -3,6 +3,7 @@ package abc;
 public class abc {
 
     public static void main(String[] args) {
-        System.out.print("abc");
+        System.out.print("abc1");
+        System.out.print("abc2");
     }
 }
